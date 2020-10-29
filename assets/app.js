2a3eb9ea064239ce2ddea158b42915d7f5791f13
domi41 <dominique.merle.41@gmail.com>
2020-10-29 08:01:09 +0100

    docs: javascriptophobia

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,10 @@
 /*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+ * There is no javascript in this project,
+ * and any js we'd add must not hinder accessibility.
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
 
 console.info('This website works best with javascript disabled.  Please disable javascript.');
