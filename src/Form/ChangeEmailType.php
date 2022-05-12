de412265062eaff35b4ecb1b6502148bda5e922c
domi41 <dominique.merle.41@gmail.com>
2022-05-12 03:17:26 +0200

    chore(clean): comment out and remove snippets for future usage
    … but are we going to use msgphp-user in the end ?
    I would like to, I think.

--- src/Form/ChangeEmailType.php
+++ src/Form/ChangeEmailType.php
@@ -4,20 +4,20 @@ declare(strict_types=1);
 
 namespace App\Form;
 
-use MsgPhp\User\Infrastructure\Validator\UniqueUsername;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\NotBlank;
-
-final class ChangeEmailType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('email', EmailType::class, [
-            'label' => 'label.username',
-            'constraints' => [new NotBlank(), new Email(), new UniqueUsername()],
-        ]);
-    }
-}
+//use MsgPhp\User\Infrastructure\Validator\UniqueUsername;
+//use Symfony\Component\Form\AbstractType;
+//use Symfony\Component\Form\Extension\Core\Type\EmailType;
+//use Symfony\Component\Form\FormBuilderInterface;
+//use Symfony\Component\Validator\Constraints\Email;
+//use Symfony\Component\Validator\Constraints\NotBlank;
+//
+//final class ChangeEmailType extends AbstractType
+//{
+//    public function buildForm(FormBuilderInterface $builder, array $options)
+//    {
+//        $builder->add('email', EmailType::class, [
+//            'label' => 'label.username',
+//            'constraints' => [new NotBlank(), new Email(), new UniqueUsername()],
+//        ]);
+//    }
+//}
